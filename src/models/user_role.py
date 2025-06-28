b1021120dc6ce8a8f538e7e4adf4b2bbfc50c1f6
aee4 <eeyramagbetor@gmail.com>
2025-06-28 00:46:28 +0000

    feat: add user role models

---
+++ src/models/user_role.py
@@ -0,0 +1,28 @@
+"""User role models"""
+
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import Field
+
+from src.models.base import CoreModel, TimestampMixin, DeleteMixin
+
+
+class UserRoleBase(CoreModel):
+    """Base UserRole Model"""
+
+    role_id: UUID = Field(..., description="ID of role")
+    user_id: UUID = Field(..., description="ID of user")
+    assigned_at: datetime = Field(default_factory=datetime.utcnow, description="Timestamp when the role was assigned")
+
+
+class UserRoleCreate(UserRoleBase):
+    pass
+
+
+class UserRolePublic(UserRoleBase, TimestampMixin):
+    pass
+
+
+class UserRoleInDb(UserRolePublic,DeleteMixin):
+    pass
