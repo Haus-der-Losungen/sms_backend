97a506ca579cbc429f5b1255db7fef5adbf38efa
aee4 <eeyramagbetor@gmail.com>
2025-06-29 20:42:18 +0000

    feat: middleware config

---
+++ src/api/middleware.py
@@ -0,0 +1,34 @@
+"""Middleware config"""
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
+import logging
+
+request_logger = logging.getLogger("request")
+
+
+def setup_middleware(app: FastAPI) -> None:
+    origins =["*"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+    @app.middleware("http")
+    async def log_requests(request: Request, call_next: callable) -> JSONResponse:
+        client_ip = request.client.host
+        method = request.method
+        path = request.url.path
+        print(f"Incoming request: {method} {path} from {client_ip}")
+        request_logger.info(f"Incoming request: {method} {path} from {client_ip}")
+
+        response = await call_next(request)
+
+        status_code = response.status_code
+        request_logger.info(f"Response: {status_code}")
+
+        return response
