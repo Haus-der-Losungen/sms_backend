4ba14ec970c31cf56d04731afdea14545dc92d6f
aee4 <eeyramagbetor@gmail.com>
2025-06-29 20:44:23 +0000

    feat: database dependency

---
+++ src/api/dependencies/database.py
@@ -0,0 +1,22 @@
+"""Dependency for db."""
+
+import logging
+from typing import Callable
+from databases import Database
+from fastapi import Depends, Request
+from src.db.repos.base import BaseRepository
+
+app_logger = logging.getLogger("app")
+
+
+def get_database(request: Request) -> Database:
+    db = getattr(request.app.state, "_db", None)
+    if db is None:
+        raise RuntimeError("Db connection not initialized.")
+    return db
+
+
+def get_repository(repo_type: BaseRepository) -> Callable:
+    def get_repo(db: Database = Depends(get_database)) -> BaseRepository:
+        return repo_type(db)
+    return get_repo
