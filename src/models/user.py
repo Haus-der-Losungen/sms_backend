0b3abf05670d5f99acef83eff4645ab7ea05baca
aee4 <eeyramagbetor@gmail.com>
2025-07-08 00:04:26 +0000

    fix: pin_hash

--- src/models/user.py
+++ src/models/user.py
@@ -1,32 +1,26 @@
 """
 User Model
 """
+from ast import pattern
 from typing import Optional
-from pydantic import BaseModel, EmailStr, field_validator, Field
+from pydantic import  BaseModel, constr, field_validator, Field
 
 from src.models.base import CoreModel, TimestampMixin, DeleteMixin, UserIDModelMixin
 
 class UserBase(CoreModel):
-    email: EmailStr = Field(..., description="user email")
     role: str = Field(..., description="user role")
 
-    @field_validator("email")
-    @classmethod
-    def lowercase_email(cls, value: str):
-        return value.lower()
-
 class UserCreate(UserBase):
-    password_hash: str = Field(..., description="hashed user password")
+    pin_hash: str = Field(min_length=6,max_length=6, pattern=r"^\d+$", description="user pin")
 
 class UserUpdate(CoreModel):
-     email: EmailStr = Field(..., description="updated user email")
+    pin_hash: str = Field(min_length=6,max_length=6, pattern= r"^\d+$", description="user pin")
 
 class UserLogin(BaseModel):
-    email: EmailStr = Field(..., description="User's email address")
-    password: str = Field(..., min_length=6, description="User's password")
+    pin_hash: str = Field(min_length=6,max_length=6, pattern= r"^\d+$", description="user pin")
 
 class UserPublic(TimestampMixin, DeleteMixin, UserBase, UserIDModelMixin):
     role: Optional[str] = Field(None)
 
 class UserInDb(UserPublic):
-    password_hash : str = Field(..., description ="Hashed user password" )
+    pin_hash: str = Field( min_length=6,max_length=6, pattern=r"^\d+$", description="user pin")
