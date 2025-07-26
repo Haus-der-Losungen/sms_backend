559825dad0ab7d89e381f08c37a71e6da7eac219
Eyram Agbetor <eeyramagbetor@gmail.com>
2025-07-26 15:53:17 +0000

    feat: add admin routes

--- src/api/routes/__init__.py
+++ src/api/routes/__init__.py
@@ -9,6 +9,7 @@ def setup_routes(app: FastAPI) -> None:
     """Configure all application routes."""
     from src.api.routes.user import user_router
     from src.api.routes.profile import profile_router
+    from src.api.routes.admin import admin_router
   
 
 
@@ -17,6 +18,7 @@ def setup_routes(app: FastAPI) -> None:
 
     app.include_router(user_router, prefix=f"{api_prefix}/user", tags=["users"])
     app.include_router(profile_router, prefix=f"{api_prefix}/profile", tags=["profile"])
+    app.include_router(admin_router, prefix=f"{api_prefix}/admin", tags=["admin"])
   
    
     
